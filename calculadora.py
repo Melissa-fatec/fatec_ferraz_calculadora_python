35e93af040fdbe30cc16dc23635d43ca746ec3f7
MELISSA MENEZES DA SILVA <melissa.silva@wds.local>
2020-02-18 10:46:31 -0300

    calculadora soma dois valores

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,3 @@
-a = int (input ("digite o primeiro operando"))
-b = int (input ("digite o primeiro operando"))
-print (f'{a} + {b} = {a + b})
+a = int(input (f' {a} + {b} = {a + b}))
+b = int(input (f' {a} + {b} = {a + b}))
+print (a + b)
